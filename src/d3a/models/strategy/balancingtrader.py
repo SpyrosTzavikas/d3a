7ac63fc933ade5ac698027d938dcc72974f3061c
Oliver Bünting <oliver@brainbot.com>
2017-09-22 17:48:43 +0200

    Added balancingtrader who buys between 0%(if total traded energy is smaller than 20Wh) and 15% of total traded energy as balancing energy

---
+++ src/d3a/models/strategy/balancingtrader.py
@@ -0,0 +1,83 @@
+from collections import defaultdict
+from typing import Dict  # noqa
+
+from d3a.exceptions import MarketException
+from d3a.models.market import Market  # noqa
+from d3a.models.strategy.base import BaseStrategy
+
+
+class BalancingtraderStrategy(BaseStrategy):
+    def __init__(self):
+        super().__init__()
+        self.next_market = None
+        self.bought_energy = defaultdict(tuple)  # type: Dict[Market, tuple(float, float)]
+
+    def event_activate(self):
+        pass
+
+    def event_tick(self, *, area):
+        pass
+
+    def event_market_cycle(self):
+        pass
+
+    def event_trade(self, *, market, trade):
+        # FIXME area will change if Balancing trader becomes part of the IAA
+        if trade.buyer == self.owner.name:
+            return
+
+        if market.area == self.area:
+            needed_balancing_energy = sum(t.offer.energy * 0.1 for t in market.trades)
+            total_trading_volume = 0
+            # Since we're buying the offers with more than the wanted 10% volume of the
+            # initial trade we might want to skip buying sometimes - If we have more
+            # than 15% of the total trade volume as balancing energy
+            for t in market.trades:
+                if t.buyer != self.owner.name:
+                    total_trading_volume += t.offer.energy
+
+            for cheapest_offer in market.sorted_offers:
+                # If there are no values in the dict for the key "markets"
+                # it raises an value error
+                try:
+                    bought_energy, spent_money = self.bought_energy[market]
+                except ValueError:
+                    bought_energy, spent_money = 0, 0
+
+                if bought_energy > (total_trading_volume * 0.15):
+                    return
+                else:
+                    needed_balancing_energy -= bought_energy
+
+                if cheapest_offer.energy < needed_balancing_energy:
+                    try:
+                        self.accept_offer(offer=cheapest_offer, market=market)
+                        self.bought_energy[market] = (bought_energy + cheapest_offer.energy,
+                                                      spent_money + cheapest_offer.price)
+                        self.log.error(
+                            "Balancetrader bought %s while the trade vol. was %s on market %s",
+                            self.bought_energy[market], total_trading_volume, market.time_slot)
+                        needed_balancing_energy -= cheapest_offer.energy
+                        continue
+                    except MarketException:
+                        # Offer already gone etc., try next one.
+                        self.log.exception("Couldn't buy")
+                        continue
+
+                if ((total_trading_volume * 0.15) - bought_energy) > \
+                        cheapest_offer.energy > needed_balancing_energy:
+                    try:
+                        self.accept_offer(offer=cheapest_offer, market=market)
+                        self.bought_energy[market] = (bought_energy + cheapest_offer.energy,
+                                                      spent_money + cheapest_offer.price)
+                        self.log.error(
+                            "Balancetrader bought %s while the trade vol. was %s on market %s",
+                            self.bought_energy[market], total_trading_volume, market.time_slot)
+                        return
+                    except MarketException:
+                        # Offer already gone etc., try next one.
+                        self.log.exception("Couldn't buy")
+                        continue
+
+                else:
+                    continue
