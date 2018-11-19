aa97ec866f14c78434a4d673fbee9c09e02d4c9d
dirkbig <dirkvandenbiggelaar@gmail.com>
2018-11-19 16:38:26 +0100

    - dataset_LES_conversion.py: code necessary to convert dataset stedin into something we can work with - data set for all household(devices), short version (1 day) - LES_project.py: grid-set up of the LES project HoogDalem

--- src/d3a/setup/LES_project.py
+++ src/d3a/setup/LES_project.py
@@ -1,4 +1,5 @@
-import pickle
+import os
+import csv
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
 from d3a.models.appliance.pv import PVAppliance
@@ -7,25 +8,47 @@ from d3a.models.strategy.predefined_pv import PVUserProfileStrategy
 from d3a.models.strategy.predefined_load import DefinedLoadStrategy
 from d3a.models.strategy.storage import StorageStrategy
 
-# device_registry_dict = {
-#     "H1 General Load": (32, 35),
-# }
+from d3a.models.appliance.simple import SimpleAppliance
+from d3a.models.strategy.commercial_producer import CommercialStrategy
+
+
+def csv_read_les_data(stedin_data_path):
+    data_directory = stedin_data_path + '/separated_data_LES'
+    number_of_households = len(os.listdir(data_directory))
+    assert number_of_households is 28
+
+    _stedin_dict = {}
+    for house in os.listdir(data_directory):
+        print('reading in house file from ', house)
+        if house.endswith(".csv"):
+            house_data_dict = {'DateTime': [], 'Home': [], 'BI': [], 'BO': [], 'DW': [], 'HP': [],
+                               'PV': [], 'SE': [], 'SI': [], 'WM': [], 'proposition': []}
+            with open(data_directory + '/' + house) as csv_file:
+                data_file = csv.DictReader(csv_file, delimiter=',')
+                for row in data_file:
+                    for key in house_data_dict:
+                        try:
+                            data_point = float(row[key])
+                        except ValueError:
+                            data_point = str(row[key])
+
+                        house_data_dict[key].append(data_point)
+            house_id = float(house_data_dict['Home'][0])
+            _stedin_dict[house_id] = house_data_dict
+    print('finished reading in LES data into dictionary')
+
+    return _stedin_dict
 
 
 def get_setup(config):
-    # Two sided market
+    # one sided market
     ConstSettings.IAASettings.MARKET_TYPE = 1
     ConstSettings.StorageSettings.CAPACITY = 1
-
-    # DeviceRegistry.REGISTRY = device_registry_dict
+    grid_connection = True
 
     """ loading in the LES data dictionary """
-    def load_obj(path, name):
-        with open(path + 'LES_data_dict/' + name + '.pkl', 'rb') as f:
-            return pickle.load(f)
-
-    stedin_data_path = './src/d3a/resources/LES_resources/'
-    stedin_dict = load_obj(stedin_data_path, 'LES_dict')
+    stedin_data_path = './src/d3a/resources/LES_resources'
+    stedin_dict = csv_read_les_data(stedin_data_path)
 
     pv_profiles = {}
     heatpump_profiles = {}
@@ -33,7 +56,7 @@ def get_setup(config):
     washingmachine_profiles = {}
     proposition = {}
 
-    list_of_houses = []
+    list_of_second_level_areas = []
 
     for house in stedin_dict:
         # house 1 id = 1 (not 0)
@@ -70,7 +93,6 @@ def get_setup(config):
             proposition[house] = {'PV': False, 'ESS': False}
 
     for house in stedin_dict:
-
         # LOADS
         house_device_list = [
             Area('H' + str(house) + ' HeatPump',
@@ -102,9 +124,17 @@ def get_setup(config):
                      appliance=SwitchableAppliance())
             )
 
-        list_of_houses.append(
+        list_of_second_level_areas.append(
             Area('House ' + str(house), house_device_list)
         )
 
-    stedin_les_community = Area('Grid', list_of_houses, config=config)
+    if grid_connection:
+        list_of_second_level_areas.append(
+            Area('Commercial Energy Producer',
+                 strategy=CommercialStrategy(energy_rate=30),
+                 appliance=SimpleAppliance()
+                 ),
+        )
+
+    stedin_les_community = Area('Grid', list_of_second_level_areas, config=config)
     return stedin_les_community
